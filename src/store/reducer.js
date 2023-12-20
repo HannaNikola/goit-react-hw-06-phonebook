57e79795d1ba048195bf4fe3bb6501f628e2feb1
HannaNikola <h.nikolaienko32@gmail.com>
2023-12-20 11:29:01 +0100

    update

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,11 +8,12 @@ export const contactReducer = (state =initialState, action) => {
     switch (action.type) {
       case ADD_CONTACT:
         return {
+          ...state,
           contacts: [...state.contacts, action.payload],
         };
       case DELETE_CONTACT:
         return {
-          
+          ...state,
           contacts: state.contacts.filter(
             contact => contact.id !== action.payload
           ),
